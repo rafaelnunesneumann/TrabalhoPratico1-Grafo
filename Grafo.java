7ba7da0b723aeaf1f3a0684d809066055058dc32
Rafael Nunes Neumann <107643462+rafaelnunesneumann@users.noreply.github.com>
2023-11-05 16:00:51 -0300

    Add files via upload

---
+++ Grafo.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class Grafo {
+
+    static class Aresta {
+        int origem, destino, peso;
+
+        public Aresta(int origem, int destino, int peso) {
+            this.origem = origem;
+            this.destino = destino;
+            this.peso = peso;
+        }
+    }
+
+    public static List<Aresta> algoritmoEdmonds(List<Aresta> arestas, int numVertices, int raiz) {
+        List<Aresta> resultado = new ArrayList<>();
+        int[] distancias = new int[numVertices];
+        int[] predecessores = new int[numVertices];
+        Arrays.fill(distancias, Integer.MAX_VALUE);
+        Arrays.fill(predecessores, -1);
+        distancias[raiz] = 0;
+
+        for (int i = 0; i < numVertices - 1; i++) {
+            for (Aresta aresta : arestas) {
+                if (distancias[aresta.origem] + aresta.peso < distancias[aresta.destino]) {
+                    distancias[aresta.destino] = distancias[aresta.origem] + aresta.peso;
+                    predecessores[aresta.destino] = aresta.origem;
+                }
+            }
+        }
+
+        for (Aresta aresta : arestas) {
+            if (distancias[aresta.origem] + aresta.peso < distancias[aresta.destino]) {
+                throw new RuntimeException("O grafo contém um ciclo negativo.");
+            }
+            resultado.add(aresta);
+        }
+
+        return resultado;
+    }
+
+    public static void imprimirArvoreGeradoraMinima(List<Aresta> arvoreGeradoraMinima) {
+        System.out.println("Árvore Geradora Mínima:");
+        int qualidade = 0;
+
+        for (Aresta aresta : arvoreGeradoraMinima) {
+            System.out.println(aresta.origem + " -> " + aresta.destino + " Peso: " + aresta.peso);
+            qualidade += aresta.peso;
+        }
+
+        System.out.println("Qualidade da Árvore: " + qualidade);
+    }
+
+    public static void main(String[] args) {
+        // Geração de um grafo totalmente conectado com pesos aleatórios
+        List<Aresta> arestas = gerarGrafoCompleto(2500);
+
+        // Execução do algoritmo de Edmonds
+        long inicio = System.currentTimeMillis();
+        List<Aresta> arvoreGeradoraMinima = algoritmoEdmonds(arestas, 2500, 0);
+        long fim = System.currentTimeMillis();
+
+        // Imprimir o tempo de execução
+        System.out.println("Tempo de execução: " + (fim - inicio) + " milissegundos");
+
+        // Imprimir a árvore geradora mínima e sua qualidade
+        imprimirArvoreGeradoraMinima(arvoreGeradoraMinima);
+    }
+
+    // Função para gerar um grafo totalmente conectado com pesos aleatórios
+    private static List<Aresta> gerarGrafoCompleto(int numVertices) {
+        List<Aresta> arestas = new ArrayList<>();
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = i + 1; j < numVertices; j++) {
+                // Peso aleatório entre 1 e 10
+                int peso = (int) (Math.random() * 10) + 1;
+                arestas.add(new Aresta(i, j, peso));
+            }
+        }
+        return arestas;
+    }
+}
---
+++ Tarjan.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Tarjan {
+    static class Edge implements Comparable<Edge> {
+        int src, dest, weight;
+
+        public Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge other) {
+            return Integer.compare(this.weight, other.weight);
+        }
+    }
+
+    static class UnionFind {
+        int[] parent, rank;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    parent[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    parent[rootX] = rootY;
+                } else {
+                    parent[rootY] = rootX;
+                    rank[rootX]++;
+                }
+            }
+        }
+    }
+
+    // Função para gerar um grafo totalmente conectado com pesos aleatórios
+    private static List<Edge> gerarGrafoCompleto(int numVertices) {
+        List<Edge> arestas = new ArrayList<>();
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = i + 1; j < numVertices; j++) {
+                // Peso aleatório entre 1 e 10
+                int peso = (int) (Math.random() * 10) + 1;
+                arestas.add(new Edge(i, j, peso));
+            }
+        }
+        return arestas;
+    }
+
+    public static List<Edge> kruskal(List<Edge> edges, int numVertices) {
+        List<Edge> minimumSpanningTree = new ArrayList<>();
+        UnionFind uf = new UnionFind(numVertices);
+        Collections.sort(edges);
+
+        for (Edge edge : edges) {
+            int rootSrc = uf.find(edge.src);
+            int rootDest = uf.find(edge.dest);
+
+            if (rootSrc != rootDest) {
+                minimumSpanningTree.add(edge);
+                uf.union(rootSrc, rootDest);
+            }
+        }
+
+        return minimumSpanningTree;
+    }
+
+    public static void main(String[] args) {
+        // Exemplo de uso
+        List<Edge> edges = gerarGrafoCompleto(2500);
+
+        long inicio = System.currentTimeMillis();
+        List<Edge> minimumSpanningTree = kruskal(edges, 2500);
+        long fim = System.currentTimeMillis();
+
+        // Imprimir o tempo de execução
+        System.out.println("Tempo de execução: " + (fim - inicio) + " milissegundos");
+
+        int qualidade = 0;
+        System.out.println("Árvore Geradora Mínima:");
+        for (Edge edge : minimumSpanningTree) {
+            System.out.println(edge.src + " -> " + edge.dest + " Peso: " + edge.weight);
+            qualidade += edge.weight;
+        }
+        System.out.println("Qualidade da Árvore: " + qualidade);
+
+    }
+}
